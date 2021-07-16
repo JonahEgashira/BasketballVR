0310966cf615bffb30ca8c12821f68881fa3affe
JonahEgashira <jonahega@gmail.com>
2021-07-16 23:34:20 +0900

    created basic game

---
+++ Assets/Scripts/Ball.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour
+{
+
+    private Rigidbody _rigidbodyComponent;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _rigidbodyComponent = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log(other.gameObject.layer);
+        if (other.gameObject.layer != 8) return;
+        ScoreText.Score++;
+        Debug.Log("Money!!!");
+    }
+}
---
+++ Assets/Scripts/ScoreText.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class ScoreText : MonoBehaviour
+{
+    private TextMeshPro _scoreText;
+    public static int Score;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _scoreText = GetComponent<TextMeshPro>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        _scoreText.text = Score.ToString();
+    }
+}
