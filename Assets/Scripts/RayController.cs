501a7de2259b400c5f48a5a39fe1caddc89378ae
JonahEgashira <jonahega@gmail.com>
2021-12-11 15:31:52 +0900

    Fix bug

--- Assets/Scripts/RayController.cs
+++ Assets/Scripts/RayController.cs
@@ -7,7 +7,7 @@ public class RayController : MonoBehaviour
 {
     public Transform anchor;
     public AudioClip sound;
-    private float _maxDistance = 10000;
+    private float _maxDistance = 100;
     private LineRenderer _lineRenderer;
 
     void Start()
@@ -34,10 +34,10 @@ public class RayController : MonoBehaviour
                     target.GetComponent<MeshRenderer>().material.color = Color.red;
                 }
             }
-            else
-            {
-                _lineRenderer.SetPosition(1, ray.origin + (ray.direction * _maxDistance));
-            }
+        }
+        else
+        {
+            _lineRenderer.SetPosition(1, ray.origin + (ray.direction * _maxDistance));
         }
     }
 }
